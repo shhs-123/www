e17439cc5115c07b08da17ff0fa7060e6f7c469b
shhs-123 <shhsw123@gmail.com>
2024-07-09 19:25:40 +0900

    12커밋: 파일 전체 업로드

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Layout } from 'antd';
 import Footer from './component/commons/footer/Footer';
-import Navbar from './component/commons/navbar/Navbar';
 import LandingPage from './component/landingpage/Landingpage';
 import Whitepaper from './component/detail/Whitepaper';
-import { createGlobalStyle } from 'styled-components';
 
 
 const { Header, Content } = Layout;
@@ -13,47 +11,15 @@ const { Header, Content } = Layout;
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <Layout>
-        <GlobalStyle />
-        <Header style={{ position: 'fixed', width: '100%', padding: '0' }}>
-          <Navbar />
-        </Header>
-
-        <Content className="site-layout" style={{ marginTop: '64px', padding: '20px', width: '100%', boxSizing: 'border-box', overflow: 'auto' }}>
-
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/Whitepaper" element={<Whitepaper />} />
-          </Routes>
-        </Content>
-        <Footer />
-      </Layout>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/Whitepaper" element={<Whitepaper />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
 
-const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'HakgyoansimWoojuR';
-    src: url('/assets/fonts/HakgyoansimWoojuR.ttf') format('truetype');
-    font-weight: normal;
-    font-style: normal;
-  }
-
-  body {
-    font-family: 'HakgyoansimWoojuR', sans-serif;
-    margin: 0;
-    padding: 0;
-  }
-
-  h1, h2, h3, h4, h5, h6 {
-    font-family: 'HakgyoansimWoojuR', sans-serif;
-  }
-
-  p, a, div, span, button {
-    font-family: 'HakgyoansimWoojuR', sans-serif;
-  }
-`;
 
 
 export default App;
--- src/component/landingpage/Landingpage.js
+++ src/component/landingpage/Landingpage.js
@@ -2,11 +2,13 @@ import React from 'react';
 import catImage from '../images/cat.png';
 import mainImage from '../images/main.png';
 // import logoImage from '../images/logo.png';
+import Navbar from '../../component/commons/navbar/Navbar';
+
 
 const LandingPage = () => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh', padding: '45px', boxSizing: 'border-box' }}>
-
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh', marginTop: '80px',boxSizing: 'border-box'}}>
+      <Navbar />
       <div style={{ width: '100%', maxWidth: '1200px', marginBottom: '100px' }}>
         <img src={mainImage} alt="Main Visual" style={{ width: '100%', height: 'auto', objectFit: 'contain' }} />
       </div>
@@ -16,15 +18,24 @@ const LandingPage = () => {
           <h1>"Www우주 고양이"</h1>
           <p>"Www우주 고양이" 프로젝트는 우주 탐험을 고양이와 함께 즐기는 색다른 경험을 제안합니다. 고양이를 위한 안전한 우주선과 환경을 조성하여 고양이의 우주 탐사 정신을 자극하고자 합니다. 이를 통해 고양이 팬덤을 형성하고, 코인과 NFT를 통해 커뮤니티를 확장하고자 합니다. 함께 우주를 탐험해 보시겠어요?</p>
         </div>
-        <div style={{ width: '20%' }}>
+        <div style={{ width: '20%', marginBottom: '100px' }}>
           <img src={catImage} alt="우주 고양이" style={{ width: '100%', height: 'auto', objectFit: 'cover' }} />
         </div>
       </div>
     </div>
+
   );
 };
 
 export default LandingPage;
+
+
+
+
+
+
+
+
 
 
 
